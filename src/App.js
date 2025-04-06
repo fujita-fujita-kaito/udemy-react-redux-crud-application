a3ce289cb562ce731e917c82c7569f12ffa5d06a
fujita <learnwithfujita@gmail.com>
2025-04-06 17:50:17 +0900

    不要なファイル、画面要素を削除した

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
         <h1>
           Hello, world
         </h1>
-      </header>
     </div>
   );
 }
